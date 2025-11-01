57143951f3dbb81e63749fa4bffa8cf020595356
Minhaz <minhazabedin1@gmail.com>
2025-11-02 03:13:24 +0600

    feat: integrate mailer module with configuration and templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { SopsModule } from './modules/sops/sops.module';
@@ -8,6 +7,14 @@ import { PaymentsModule } from './modules/payments/payments.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { CommonModule } from './modules/common/common.module';
+import { CommonService } from './modules/common/common.service';
+import config from './config';
+import path from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter.js';
+
 
 @Module({
   imports: [
@@ -18,8 +25,35 @@ import { AppService } from './app.service';
     SopsModule,
     AiModule,
     PaymentsModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          service: 'gmail',
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false,
+          auth: {
+            user: config.mailer_auth_mail,
+            pass: config.mailer_auth_password,
+          },
+        },
+        defaults: {
+          from: '"WinnerSOP" <' + config.default_mail_sender + '>',
+        },
+        template: {
+          dir: path.join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+    CommonModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CommonService],
 })
 export class AppModule {}
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  mailer_auth_mail: process.env.MAILER_AUTH_MAIL,
+  mailer_auth_password: process.env.MAILER_AUTH_PASSWORD,
+  default_mail_sender: process.env.DEFAULT_MAIL_SENDER,
+};
+
+export default config;
